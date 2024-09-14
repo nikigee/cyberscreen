9d41d2d514649ddada0e6919200ca0dca0225363
Niki <ngolev.bus@gmail.com>
2024-09-14 22:43:34 +1000

    import magicdice script

--- src/components/DiceRoller.vue
+++ src/components/DiceRoller.vue
@@ -1,3 +1,7 @@
 <template>
     <h2 class="header_text text-start">運DICE</h2>
 </template>
+
+<script lang="js">
+
+</script>
--- src/main.js
+++ src/main.js
@@ -2,15 +2,23 @@ import './assets/main.scss'
 
 import 'bootstrap';
 
-import { createApp } from 'vue'
+import { createApp, reactive } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
 
+import { magicDice } from "./assets/md_magicdie"
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+// Create a reactive object for $md
+const md = reactive(magicDice);
+
+// Expose $md to all components
+app.config.globalProperties.$md = md;
 
 app.mount('#app')
