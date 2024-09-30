6bcd16ff2c44eda93e8c3b493f4f01b9515c39c2
Nikita Golev <ngolev.bus@gmail.com>
2024-09-30 17:02:22 +1000

    Update ActivityLog.vue

--- src/components/ActivityLog.vue
+++ src/components/ActivityLog.vue
@@ -2,7 +2,10 @@
     <div class="text-start">
         <div class="bg-secondary text-dark px-2">CONSOLE</div>
         <div style="height: 150px;" class="border border-secondary py-1 console">
-            <span class="entry text-muted" v-if="$cyber.log.length === 0">> cyber log start</span>
+            <div v-if="$cyber.log.length === 0">
+                <span class="entry text-muted">> cyber log start</span>
+            </div>
+            
             <div v-for="item in $cyber.log" :key="item.id">
                 <span class="entry text-secondary">> {{ item.content }}</span>
             </div>
