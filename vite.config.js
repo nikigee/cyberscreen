1fbff6eb491f5510e506337b778d00eee0a0315f
Nikita Golev <ngolev.bus@gmail.com>
2025-07-10 11:53:51 +1000

    update for vercel

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  base: process.env.NODE_ENV == "production" ? '/cyberscreen/' : '/',
+  base: '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
